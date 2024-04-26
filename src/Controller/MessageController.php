37f1935f6ab9c51047eb9d84282768a703b6fa56
Roman Nemish <roman.nemish@intelliboard.net>
2024-04-26 23:41:28 +0200

    ID validation

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -44,13 +44,13 @@ class MessageController extends AbstractController
         return $this->render('message/new.html.twig',  compact('form', 'message'));
     }
 
-    #[Route('/{id}', name: 'app_message_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_message_show', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function show(Message $message): Response
     {
         return $this->render('message/show.html.twig',  compact('message'));
     }
 
-    #[Route('/{id}/edit', name: 'app_message_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_message_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function edit(Request $request, Message $message): Response
     {
         $form = $this->createForm(MessageType::class, $message);
@@ -65,7 +65,7 @@ class MessageController extends AbstractController
         return $this->render('message/edit.html.twig', compact('form', 'message'));
     }
 
-    #[Route('/{id}', name: 'app_message_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_message_delete', requirements: ['id' => '\d+'], methods: ['POST'])]
     public function delete(Request $request, Message $message): Response
     {
         if ($this->isCsrfTokenValid('delete' . $message->getId(), $request->request->get('_token'))) {
